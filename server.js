d991dab24dd340fc21f0db2540563f9fbe06d714
Yigezu Mulugeta <Yigezumb@gmail.com>
2020-10-31 09:11:25 -0400

    addEmployees function added and tested

--- server.js
+++ server.js
@@ -91,7 +91,224 @@ function start() {
   });
 }
 // Add functions
-function addEmployees() {}
+function addEmployees() {
+  const deptT = [];
+  const deptId = [];
+  const rolesT = [];
+  const emplFn = [];
+  const emplFirstN = [];
+  const emplLastN = [];
+
+  connection.query("SELECT * FROM departments", function (err, data) {
+    if (err) throw new Error(err);
+    data.forEach((element) => {
+      deptT.push(element.name);
+      deptId.push(element.id);
+    });
+    rolesQuery();
+  });
+  function rolesQuery() {
+    connection.query("SELECT DISTINCT(title) FROM roles", function (err, data) {
+      if (err) throw new Error(err);
+      data.forEach((element) => {
+        rolesT.push(element.title);
+      });
+      employeeQuery();
+    });
+  }
+
+  function employeeQuery() {
+    connection.query(
+      `SELECT DISTINCT(concat(first_name ,' ',last_name)) manager, id, first_name, last_name FROM employees;`,
+      function (err, data) {
+        if (err) throw new Error(err);
+        data.forEach((element) => {
+          emplFn.push(element.manager);
+          emplFirstN.push(element.first_name);
+          emplLastN.push(element.last_name);
+        });
+        emplFn.push("None");
+        addNewEmp();
+      }
+    );
+  }
+
+  function addNewEmp() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "first_name",
+          message: "What is the first name of the employee?",
+          validate: function (value) {
+            if (value.trim() === "") {
+              return false;
+            }
+            return true;
+          },
+        },
+        {
+          type: "input",
+          name: "last_name",
+          message: "What is the last name of the employee?",
+          validate: function (value) {
+            if (value.trim() === "") {
+              return false;
+            }
+            return true;
+          },
+        },
+
+        {
+          type: "list",
+          message: "What is the department of the new employee?",
+          name: "department",
+          choices: deptT,
+        },
+        {
+          type: "list",
+          message: "What is the role of the new employee?",
+          name: "role",
+          choices: rolesT,
+        },
+        {
+          type: "list",
+          message: "Who is the manager of this new employee?",
+          name: "manager",
+          choices: emplFn,
+        },
+      ])
+      .then(function (resp) {
+        //  andd code here
+        const fname = resp.first_name.trim();
+        const lname = resp.last_name.trim();
+        const deprtId = deptId[deptT.indexOf(resp.department)];
+        const role = resp.role;
+        const empManager = resp.manager;
+        let manIndex = emplFn.indexOf(empManager);
+        let managerID = "";
+
+        connection.query(
+          `SELECT * FROM roles WHERE title = ? AND department_id = ?`, [role, deprtId],
+          // {
+          //   title: role,
+          //   department_id: deprtId,
+          // },
+          function (err, data) {
+            if (err) throw new Error(err);
+            const roleId = data[0].id;
+            if (manIndex === emplFirstN.length) {
+              connection.query(
+                `INSERT INTO employees SET?`,
+                {
+                  first_name: fname,
+                  last_name: lname,
+                  role_id: roleId,
+                },
+                function (err) {
+                  if (err) throw new Error(err);
+                  // some code here
+                  console.log("Employee successfully added!");
+                  whatNext();
+                }
+              );
+            } else {
+              const managerFirstName = emplFirstN[manIndex];
+              const managerLastName = emplLastN[manIndex];
+
+              connection.query(
+                "SELECT id FROM employees WHERE first_name = ? AND last_name = ? AND role_id = ?", [managerFirstName, managerLastName, roleId],
+                // {
+                //   first_name: managerFirstName,
+                //   last_name: managerLastName,
+                //   role_id: roleId,
+                // },
+                function (err, data) {
+                  if (err) throw new Error(err);
+                  if (data.length === 1) {
+                    managerID = data[0].id;
+                    connection.query(
+                      `INSERT INTO employees SET?`,
+                      {
+                        first_name: fname,
+                        last_name: lname,
+                        role_id: roleId,
+                        manager_id: managerID,
+                      },
+                      function (err) {
+                        if (err) throw new Error(err);
+                        // some code here
+                        console.log("Employee successfully added!");
+                        whatNext();
+                      }
+                    );
+                  } else if (data.length > 1) {
+                    //  code here if there are more than one manager with that information
+                    const managerIdArray = [];
+                    data.forEach((element) => {
+                      managerIdArray.push(element.id);
+                    });
+                    inquirer
+                      .prompt({
+                        type: "list",
+                        name: "man_id",
+                        message:
+                          "What is the id of the manager for this new employee?",
+                        choices: managerIdArray,
+                      })
+                      .then(function (res) {
+                        managerID = res.man_id;
+                        connection.query(
+                          "INSERT INTO employees SET?",
+                          {
+                            first_name: fname,
+                            last_name: lname,
+                            role_id: roleId,
+                            manager_id: managerID,
+                          },
+                          function (err) {
+                            if (err) throw new Error(err);
+                            // code here after finishing adding employee
+                            console.log("Employee successfully added!");
+                            whatNext();
+                          }
+                        );
+                      });
+                  } else {
+                    // if manager removed during the process handles here
+                    console.log(
+                      new Error(
+                        "Manager couldn't be found with the provided information!"
+                      )
+                    );
+                    whatNext();
+                  }
+                }
+              );
+            }
+          }
+        );
+      });
+    // after finising adding the employee what next?
+    function whatNext() {
+      inquirer
+        .prompt({
+          type: "list",
+          message: `Would you like to continue adding employees?`,
+          name: "confirm",
+          choices: ["Yes", "No"],
+        })
+        .then(function (response) {
+          const respond = response.confirm;
+          if (respond === "Yes") {
+            addEmployees();
+          } else {
+            start();
+          }
+        });
+    }
+  }
+}
 
 function addRoles() {
   connection.query("SELECT * FROM departments", function (err, data) {
@@ -134,20 +351,20 @@ function addRoles() {
           function (err) {
             if (err) throw new Error(err);
             inquirer
-            .prompt({
-              type: "list",
-              message: `Would you like to continue adding roles?`,
-              name: "confirm",
-              choices: ["Yes", "No"],
-            })
-            .then(function (response) {      
-              const respond = response.confirm;
-              if (respond === "Yes") {
-                addDepartments();
-              } else {
-                start();
-              }
-            });
+              .prompt({
+                type: "list",
+                message: `Would you like to continue adding roles?`,
+                name: "confirm",
+                choices: ["Yes", "No"],
+              })
+              .then(function (response) {
+                const respond = response.confirm;
+                if (respond === "Yes") {
+                  addDepartments();
+                } else {
+                  start();
+                }
+              });
           }
         );
       });
@@ -173,21 +390,20 @@ function addDepartments() {
         function (err) {
           if (err) throw new Error(err);
           inquirer
-          .prompt({
-            type: "list",
-            message: `Would you like to continue adding departments?`,
-            name: "confirm",
-            choices: ["Yes", "No"],
-          })
-          .then(function (response) {      
-            const respond = response.confirm;
-            if (respond === "Yes") {
-              addDepartments();
-            } else {
-              start();
-            }
-          });
-
+            .prompt({
+              type: "list",
+              message: `Would you like to continue adding departments?`,
+              name: "confirm",
+              choices: ["Yes", "No"],
+            })
+            .then(function (response) {
+              const respond = response.confirm;
+              if (respond === "Yes") {
+                addDepartments();
+              } else {
+                start();
+              }
+            });
         }
       );
     });
@@ -218,9 +434,7 @@ function removeDepartment() {}
 
 function removeRole() {}
 
-function promting(input) {
-
-}
+function promting(input) {}
 //  function to to end database connection
 function exit() {
   connection.end();
