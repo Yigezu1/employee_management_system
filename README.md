14d940f74dda290e7366edb5f5e264cd8dfe1fb8
Yigezu Mulugeta <Yigezumb@gmail.com>
2020-11-03 18:37:23 -0500

    updated the schema.sql, and the readme file

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# employee_management_system
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](#license)
+
+
+  # employee_management_system
+
+
+  ## Description
+
+  
+  This app has the functionality to add, remove, and update employees, departments and roles. The user can view all employees, departments, roles and view  employees by manager. This app also have the functionality to view total departments utilized budget. All the applications functionalities has been wired up and users can choose what they want to do after one functionality has been excuted. If they choose 'Exit' the app will exit. 
+
+
+  ## Table of Contents
+
+  
+  
+  * [Installation](#installation)
+
+  
+  * [Usage](#usage)
+
+  
+  * [License](#license)
+
+  
+  * [Contributing](#contributing)
+
+  
+  * [Tests](#tests)
+
+  
+  * [Questions](#questions)
+
+  
+
+
+  ## Installation
+
+  
+  Anyone who wants to use this app can take the source code from my GitHub repository and use it.
+
+
+  ## Usage
+
+  
+  When you start running this app you will be presented an intutive and self-descriptive options. Before runnining the app though, you need to create you databases and tables with the schema.sql file which is also in the source file.
+
+
+  ## License
+
+  
+  GNU General Public License v3.0
+
+
+  ## Contributing
+
+  
+  If you want to contribute to this app, fix or report bugs, you can reach me out using the contact information available in the 'Questions' section
+
+
+  ## Tests
+
+  
+  You can check the different senarios with this app and perform testing.
+
+      
+  ## Questions
+
+  
+  [Yigezu Birhane](https://yigezu1.github.io/Yigezu1/)
+
+  yigezu.mulu@gmail.com
+
+  You can reach me out with email, or phone available in my Github profile if you have any question.
+  
--- schema.sql
+++ schema.sql
@@ -27,3 +27,38 @@ CREATE TABLE roles (
     PRIMARY KEY (id)
 );
 
+INSERT INTO departments (name) values ("computer science");
+INSERT INTO departments (name) values ("physics");
+INSERT INTO departments (name) values ("chemistry");
+
+-- This will insert into roles table
+
+INSERT INTO roles (title, salary, department_id) values ("lead", 100000, 1);
+INSERT INTO roles (title, salary, department_id) values ("head", 120000,1);
+INSERT INTO roles (title, salary, department_id) values ("admin associate", 60000,1);
+
+INSERT INTO roles (title, salary, department_id) values ("lead", 100000, 2);
+INSERT INTO roles (title, salary, department_id) values ("head", 120000,2);
+INSERT INTO roles (title, salary, department_id) values ("admin", 70000,2);
+
+INSERT INTO roles (title, salary, department_id) values ("lead", 100000, 3);
+INSERT INTO roles (title, salary, department_id) values ("head", 120000,3);
+INSERT INTO roles (title, salary, department_id) values ("senior admin", 80000,3);
+
+-- add employees to the employee table
+
+INSERT INTO employees (first_name, last_name, role_id) values ("yigezu", "birhane", 2);
+INSERT INTO employees (first_name, last_name, role_id) values ("hana", "aklilu", 5);
+INSERT INTO employees (first_name, last_name, role_id) values ("naod", "yigezu", 8);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id) values ("haset", "yigezu", 1, 1);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) values ("saba", "joe", 3, 4);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) values ("tigist", "adinew", 3, 4);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id) values ("helen", "melese", 4, 2);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) values ("deme", "aklilu", 6, 7);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) values ("alem", "ashagre", 6, 7);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id) values ("john", "chris", 7, 3);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) values ("suzzie", "tomson", 9, 10);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) values ("faith", "john", 9, 10);
